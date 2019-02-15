12384b2caf2193570f99e2c8d222e77ebd573fba
Yifan Pan <4anpan@gmail.com>
2019-02-15 17:22:29 +0800

    #263

---
+++ leetcode/263_ugly_number.cpp
@@ -0,0 +1,23 @@
+#include "iostream"
+
+using namespace std;
+
+class Solution {
+public:
+    bool isUgly(int num) {
+        if (num <= 0) {
+          return false;
+        }
+        while (num % 2 == 0 && (num /= 2));
+        while (num % 3 == 0 && (num /= 3));
+        while (num % 5 == 0 && (num /= 5));
+        return num == 1;
+    }
+};
+
+int main() {
+  Solution s{};
+  for (int i = 0; i < 100; i++) {
+    cout << i << "\t" << s.isUgly(i) << endl;
+  }
+}
