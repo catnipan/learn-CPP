25b6be862c0426f5fdf6658e99a112b69dcb1598
Yifan Pan <4anpan@gmail.com>
2019-02-17 20:42:44 +0800

    refactor

--- compiler/recursive_descent_parser.cpp
+++ compiler/recursive_descent_parser.cpp
@@ -3,102 +3,125 @@
 
 using namespace std;
 
-inline void report_error() {
-  throw string("syntax error");
-}
-
-string::const_iterator lookahead;
-
-void match(const char& chr) {
-  if (*lookahead == chr) {
-    lookahead++;
-  } else {
-    report_error();
+class Parser {
+public:
+  void run(const string& s) {
+    lookahead = s.begin();
+    try {
+      parse(s);
+      if (lookahead != s.end()) {
+        report_error();
+      }
+      cout << "parse '" << s << "' : ok" << endl;
+    } catch (const string& err) {
+      cout << "parse '" << s << "' : " << err << endl;
+    }
   }
-}
-
-// S1 -> + S1 S1 | - S1 S1 | a
-void S1(){
-  switch (*lookahead) {
-    case '+':
-      match('+'); S1(); S1();
-      break;
-    case '-':
-      match('-'); S1(); S1();
-      break;
-    case 'a':
-      match('a');
-      break;
-    default:
+protected:
+  virtual void parse(const string& s) = 0;
+  string::const_iterator lookahead;
+  void report_error() {
+    throw string("syntax error");
+  }
+  void match(const char& chr) {
+    if (*lookahead == chr) {
+      lookahead++;
+    } else {
       report_error();
-      throw;
+    }
   }
-}
-void parser1(const string& s) {
-  lookahead = s.begin();
-  S1();
-  if (lookahead != s.end()) {
-    report_error();
-  }
-}
+};
 
-// S2 -> S2 ( S2 ) S2 | e
-// S2' -> ( S2' ) S2' | e
-
-void S2() {
-  switch (*lookahead) {
-    case '(':
-      match('(');
-      S2();
-      match(')');
-      S2();
-      break;
-    default:
-      break;
+// S -> + S S | - S S | a
+class Parser1: public Parser {
+private:
+  virtual void parse(const string& s) {
+    S();
   }
-}
-void parser2(const string& s) {
-  lookahead = s.begin();
-  S2();
-  if (lookahead != s.end()) {
-    report_error();
+  void S(){
+    switch (*lookahead) {
+      case '+':
+        match('+'); S(); S();
+        break;
+      case '-':
+        match('-'); S(); S();
+        break;
+      case 'a':
+        match('a');
+        break;
+      default:
+        report_error();
+        throw;
+    }
   }
-}
+};
 
-// S3 -> 0 S3 1 | 0 1
-// S3' -> 0 A
+// S -> S ( S ) S | e
+// S' -> ( S' ) S' | e
+class Parser2: public Parser {
+private:
+  virtual void parse(const string& s) {
+    S();
+  }
+  void S() {
+   switch (*lookahead) {
+      case '(':
+        match('(');
+        S();
+        match(')');
+        S();
+        break;
+      default:
+        break;
+    }
+  }
+};
+
+// S -> 0 S 1 | 0 1
+// S' -> 0 A
 // A -> 1 | 0 A 1
-void A() {
-  switch (*lookahead)
-  {
-    case '1':
-      match('1');
-      break;
-    case '0':
-      match('0');
-      A();
-      match('1');
-      break;
-    default:
-      report_error();
-      break;
+class Parser3: public Parser {
+private:
+  virtual void parse(const string& s) {
+    S();
   }
-}
-void parser3(const string& s) {
-  lookahead = s.begin();
-  match('0');
-  A();
-  if (lookahead != s.end()) {
-    report_error();
+  void S() {
+    match('0');
+    A();
   }
-}
+  void A() {
+    switch (*lookahead)
+    {
+      case '1':
+        match('1');
+        break;
+      case '0':
+        match('0');
+        A();
+        match('1');
+        break;
+      default:
+        report_error();
+        break;
+    }
+  }
+};
 
 int main() {
-  try {
-    // parser1("+aaa");
-    parser2("()()(())");
-    // parser3("00001111");
-  } catch (const string& error) {
-    cout << error << endl;
-  }
+  Parser1 p1{};
+  p1.run("+aa");
+  p1.run("+aaa");
+  p1.run("+a-aa");
+
+  Parser2 p2{};
+  p2.run("(())()()");
+  p2.run("(()())");
+  p2.run("()())");
+  p2.run("()(");
+
+  Parser3 p3{};
+  p3.run("001");
+  p3.run("01");
+  p3.run("000111");
+  p3.run("0111");
 }
